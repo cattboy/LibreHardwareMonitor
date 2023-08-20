cdf935aca061fd9c8d558fbe905f25cf0fb62414
cattboy <cattboy@gmail.com>
2023-08-20 18:01:09 +1000

    Changes for controlling ITE IT8792E. X570 AORUS MASTER, fans in https://github.com/Rem0o/FanControl.Releases System Fan #.5 Pump System Fan #.6 Pump System Fan #.4
    https://github.com/LibreHardwareMonitor/LibreHardwareMonitor/issues/251#issuecomment-1684889818

--- LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IT87XX.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IT87XX.cs
@@ -72,7 +72,8 @@ internal class IT87XX : ISuperIO
 
         FAN_PWM_CTRL_REG = chip switch
         {
-            Chip.IT8665E or Chip.IT8625E =>new byte[] { 0x15, 0x16, 0x17, 0x1e, 0x1f, 0x92 },
+            Chip.IT8665E or Chip.IT8625E => new byte[] { 0x15, 0x16, 0x17, 0x1e, 0x1f, 0x92 },
+            Chip.IT8792E => new byte[] { 0x15, 0x16, 0x17 },
             _ => new byte[] { 0x15, 0x16, 0x17, 0x7f, 0xa7, 0xaf }
         };
 
@@ -191,8 +192,8 @@ internal class IT87XX : ISuperIO
         _voltageGain = chip switch
         {
             Chip.IT8613E or Chip.IT8620E or Chip.IT8628E or Chip.IT8631E or Chip.IT8721F or Chip.IT8728F or Chip.IT8771E or Chip.IT8772E or Chip.IT8686E or Chip.IT8688E or Chip.IT8689E => 0.012f,
-            Chip.IT8625E or Chip.IT8792E or Chip.IT87952E => 0.011f,
-            Chip.IT8655E or Chip.IT8665E => 0.0109f,
+            Chip.IT8625E or Chip.IT87952E => 0.011f,
+            Chip.IT8655E or Chip.IT8665E or Chip.IT8792E => 0.0109f,
             _ => 0.016f
         };
 
